a4a945853d4c683563a8c75f4c93931f32c8471b
Kirill <igkirill2323@yandex.ru>
2026-02-17 09:46:46 +0700

    url patterns incidets's endpoints has been fixed

--- backend/apps/incidents/urls.py
+++ backend/apps/incidents/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'incidents', views.IncidentViewSet, basename='incident')
+router.register(r'', views.IncidentViewSet, basename='incident')
 router.register(r'types', views.IncidentTypeViewSet)
 router.register(r'statuses', views.IncidentStatusViewSet)
 
